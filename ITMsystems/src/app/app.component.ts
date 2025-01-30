cb4d0dc48d0b7d5e5e017215d643243ecd700310
Prometeya <polina.knudsen@gmail.com>
2025-01-30 10:14:12 +0100

    itms-v11

--- ITMsystems/src/app/app.component.ts
+++ ITMsystems/src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HelpDeskComponent } from './help-desk/help-desk.component';
 import { HeaderComponent } from './header/header.component';
-
-
-
 
 @Component({
   selector: 'app-root',
