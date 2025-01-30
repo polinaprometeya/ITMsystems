8116d830b35a6ae832d29daab276cf1b2a6101be
Prometeya <polina.knudsen@gmail.com>
2025-01-30 08:25:02 +0100

    itms-v7

--- ITMsystems/src/app/app.component.ts
+++ ITMsystems/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelpDeskComponent } from './help-desk/help-desk.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelpDeskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
