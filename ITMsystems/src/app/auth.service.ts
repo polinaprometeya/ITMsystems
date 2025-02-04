e7c7fb7f064e250619c98c43baa1855ac6439c50
Prometeya <polina.knudsen@gmail.com>
2025-02-04 22:43:35 +0100

    itms-v29

--- ITMsystems/src/app/auth.service.ts
+++ ITMsystems/src/app/auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, first, catchError } from 'rxjs';
+import { User } from './models/user';
+import { application } from 'express';
+import { ErrorHandlerService } from './error-handler.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private url = "http://localhost:4200/auth/signup";
+  
+  httpOptions:{ headers: HttpHeaders} = {
+  headers: new HttpHeaders({"Content-Type": "application/json"}),
+};
 
-  constructor() { }
+  constructor(private http: HttpClient, private errorHandlerService: ErrorHandlerService) { }
+
+  signup(user: Omit<User, "id">): Observable<User>{
+    return this.http.post<User>(this.url, user, this.httpOptions).pipe(
+      first(),
+      catchError(this.errorHandlerService.handleError<User>("signup"))
+    )
+  }
 }
